d3766fb837b217f737411db5384fad1a18fb70fe
RetroHax <tflash404@gmail.com>
2026-08-16 14:27:42 -0500

    idc

--- log_extract.sh
+++
@@ -1,7 +0,0 @@
-#!/usr/bin/env bash
-awk -v start="$1" -v end="$2" '
-{
-    ts = substr($0, 1, 19)
-    if (ts >= start && ts <= end && (/DEBUG registration/ || /DEBUG No data returned/)) print
-}
-' $3
